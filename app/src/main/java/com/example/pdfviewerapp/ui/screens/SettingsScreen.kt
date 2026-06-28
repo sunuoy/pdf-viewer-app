9dab31c5a760c3504477429b29efb5cf06707c83
sunuoy <153935091+sunuoy@users.noreply.github.com>
2026-06-29 01:52:30 +0530

    Set versionCode to 3 and versionName to 3.0.0

--- app/src/main/java/com/example/pdfviewerapp/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/pdfviewerapp/ui/screens/SettingsScreen.kt
@@ -164,7 +164,7 @@ fun SettingsScreen(
                     }
                     Spacer(modifier = Modifier.height(8.dp))
                     Text(
-                        text = "Version 3.5.8",
+                        text = "Version 3.0.0",
                         fontWeight = FontWeight.Bold,
                         color = MaterialTheme.colorScheme.primary,
                         style = MaterialTheme.typography.titleSmall
